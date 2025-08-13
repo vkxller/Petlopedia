44150e6db966e1b4b6b151601af744d3e53beae6
vKxller <jordangamer1237@gmail.com>
2025-08-12 23:52:43 -0400

    m

--- js/script.js
+++ js/script.js
@@ -3,20 +3,20 @@ document.getElementById("iniciar").addEventListener("click", function() {
     //Acá llamamos a la función cambiarColor
 });
 
-let status = true;
 
 // Acá tenemos las instrucciones para el cambio de color 
 function cambiarColor() {
     let boton = document.getElementById("iniciar"); /*Obtencion y pasado a string del color*/
-    if (boton.style.backgroundColor.toString() === "rgb(240, 193, 75)") {
-        boton.style.backgroundColor = "rgb(255, 38, 0)"
-        boton.innerText = "Cerrar Sesion"
+    if (boton.style.backgroundColor.toString() === "rgb(240, 193, 75)") { // Verificamos si el color es igual al que queremos cambiar
+        boton.style.backgroundColor = "rgb(255, 38, 0)" // El color cambia a roo al igual que el contenido del boton(el texto)
+        boton.innerText = "Cerrar Sesion" // Cambiamos A cerrar Sesion
     } else {
-        boton.style.backgroundColor = "rgb(240, 193, 75)"
-        boton.innerText = "Iniciar Sesion"
+        boton.style.backgroundColor = "rgb(240, 193, 75)" // Si no, el color cambia a amarillo y su texto igualmente
+        boton.innerText = "Iniciar Sesion" // Cambia su txto igual
     }
-    console.log(boton.style.backgroundColor);
+   // console.log(boton.style.backgroundColor); // Lo aplicamos para verificar funcionamiento del boton
 }
+
 
 // Eliminacion de boton al apretar click en este
 document.getElementById("agregarDefinicion").addEventListener("click", function() {
